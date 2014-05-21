eea2bbabcbc25e17fd7a5a5af6098ac92ce3ed5e
jamesbonder <jambonder@gmail.com>
2014-05-21 10:27:15 +0100

    Content Changes
    Added Animation, Art & Design course information and links

---
+++ js/carousels2.js
@@ -0,0 +1,144 @@
+// JavaScript Document
+
+	$(function() {
+				
+/*	CarouFredSel: a circular, responsive jQuery carousel.
+	Configuration created by the "Configuration Robot"
+	at caroufredsel.dev7studios.com
+*/
+$("#carousel").carouFredSel({
+	width: 800,
+	height: "auto",
+	items: {
+		visible: 4,
+		minimum: 4,
+		width: 200,
+		height: 200
+	},
+	auto: false,
+	prev: {
+		button: "#prev_btn1",
+		key: "left"
+	},
+	next: {
+		button: "#next_btn1",
+		key: "right"
+	}
+	
+	
+	
+});
+
+
+			});
+	
+			$(function() {
+				
+/*	CarouFredSel: a circular, responsive jQuery carousel.
+	Configuration created by the "Configuration Robot"
+	at caroufredsel.dev7studios.com
+*/
+$("#carousel2").carouFredSel({
+	width: 800,
+	height: "auto",
+	items: {
+		visible: 4,
+		minimum: 4,
+		width: 200,
+		height: 200
+	},
+	auto: false,
+	prev: {
+		button: "#prev_btn2",
+		key: "left"
+	},
+	next: {
+		button: "#next_btn2",
+		key: "right"
+	}
+});
+			});
+		
+		$(function() {
+				
+/*	CarouFredSel: a circular, responsive jQuery carousel.
+	Configuration created by the "Configuration Robot"
+	at caroufredsel.dev7studios.com
+*/
+$("#carousel3").carouFredSel({
+	width: 800,
+	height: "auto",
+	items: {
+		visible: 4,
+		minimum: 4,
+		width: 200,
+		height: 200
+	},
+	auto: false,
+	prev: {
+		button: "#prev_btn3",
+		key: "left"
+	},
+	next: {
+		button: "#next_btn3",
+		key: "right"
+	}
+});
+		
+});
+
+$(function() {
+				
+/*	CarouFredSel: a circular, responsive jQuery carousel.
+	Configuration created by the "Configuration Robot"
+	at caroufredsel.dev7studios.com
+*/
+$("#carousel4").carouFredSel({
+	width: 800,
+	height: "auto",
+	items: {
+		visible: 4,
+		minimum: 4,
+		width: 200,
+		height: 200
+	},
+	auto: false,
+	prev: {
+		button: "#prev_btn4",
+		key: "left"
+	},
+	next: {
+		button: "#next_btn4",
+		key: "right"
+	}
+});
+		
+});
+
+$(function() {
+				
+/*	CarouFredSel: a circular, responsive jQuery carousel.
+	Configuration created by the "Configuration Robot"
+	at caroufredsel.dev7studios.com
+*/
+$("#carousel5").carouFredSel({
+	width: 800,
+	height: "auto",
+	items: {
+		visible: 4,
+		minimum: 4,
+		width: 200,
+		height: 200
+	},
+	auto: false,
+	prev: {
+		button: "#prev_btn5",
+		key: "left"
+	},
+	next: {
+		button: "#next_btn5",
+		key: "right"
+	}
+});
+		
+});
